328d0b5784699c72f71b4c5911b65412cb4bae01
nourmt <mtarnour7@gmail.com>
2023-01-26 11:43:29 +0100

    work

---
+++ main.js
@@ -0,0 +1,264 @@
+const data=[
+{
+    article:"casual clothers",
+    prix:160,
+    réduction:20,
+    genre:"men",
+    src:"/image/1 men.jpg"
+
+},
+
+{article:"casual clothers",
+prix:100,
+réduction:20,
+genre:"women",
+src:"/image/1.jpg"
+
+},
+
+{article:"casual clothers",
+prix:90,
+réduction:20,
+genre:"men",
+src:"/image/ 2men.jpg"
+
+},
+
+{article:"casual clothers",
+prix:100,
+réduction:20,
+genre:"women",
+src:"/image/2.jpg"
+
+},
+{
+    article:"casual clothers",
+    prix:200,
+    réduction:30,
+    genre:"men",
+    src:"/image/3 men.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:150,
+    réduction:20,
+    genre:"women",
+    src:"/image/3.jpg"
+
+},
+{
+    article:"casual clothers",
+    prix:210,
+    réduction:30,
+    genre:"men",
+    src:"/image/4 men.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:250,
+    réduction:30,
+    genre:"women",
+    src:"/image/4.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:199,
+    réduction:30,
+    genre:"men",
+    src:"/image/5 men.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:170,
+    réduction:20,
+    genre:"women",
+    src:"/image/5.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:140,
+    réduction:20,
+    genre:"men",
+    src:"/image/6 men.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:100,
+    réduction:20,
+    genre:"women",
+    src:"/image/6.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:160,
+    réduction:20,
+    genre:"women",
+    src:"/image/6.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:180,
+    réduction:30,
+    genre:"men",
+    src:"/image/7 men.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:300,
+    réduction:50,
+    genre:"women",
+    src:"/image/7.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:210,
+    réduction:30,
+    genre:"men",
+    src:"/image/8 men.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:150,
+    réduction:20,
+    genre:"women",
+    src:"/image/8.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:180,
+    réduction:30,
+    genre:"men",
+    src:"/image/9 men.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:299,
+    réduction:30,
+    genre:"women",
+    src:"/image/9.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:200,
+    réduction:20,
+    genre:"women",
+    src:"/image/10.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:160,
+    réduction:20,
+    genre:"kids",
+    src:"/image/kids 1.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:99,
+    réduction:20,
+    genre:"kids",
+    src:"/image/kids 2.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:110,
+    réduction:30,
+    genre:"kids",
+    src:"/image/kids 3.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:110,
+    réduction:20,
+    genre:"kids",
+    src:"/image/kids 4.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:200,
+    réduction:50,
+    genre:"kids",
+    src:"/image/kids 5.jpg",
+
+},
+{
+    
+    article:"casual clothers",
+    prix:140,
+    réduction:30,
+    genre:"kids",
+    src:"/image/kids 6.jpg"
+
+},
+{
+    
+    article:"casual clothers",
+    prix:160,
+    réduction:30,
+    genre:"kids",
+    src:"/image/kids 7.jpg"
+
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
+
